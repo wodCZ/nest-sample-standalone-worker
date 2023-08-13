94192971d294a919feaef864c422402805aff023
Martin Janeček <admin@ikw.cz>
2023-08-13 13:41:37 +0200

    feat: add bull

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        // run docker compose up -d to start redis
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
